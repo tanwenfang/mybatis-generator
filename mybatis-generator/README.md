3a8cb8a43dfc589ba7df90e0e28de8f43275df79
tanwenfang <1643327713@qq.com>
2019-02-19 16:06:39 +0800

    Java逆向工程

---
+++ mybatis-generator/README.md
@@ -0,0 +1,7 @@
+1.生成代码 请执行generatorConfig.xml 文件
+
+maven执行 参数：
+base directory ：${workspace_loc:/mybatis-generator}
+goals：mybatis-generator:generate
+其他放空
+
---
+++ mybatis-generator/src/main/java/com/twf/book/product/persistence/ProductMapper.java
@@ -0,0 +1,7 @@
+package com.twf.book.product.persistence;
+
+import com.twf.book.product.MyMapper;
+import com.twf.book.product.domain.Product;
+
+public interface ProductMapper extends MyMapper<Product> {
+}
