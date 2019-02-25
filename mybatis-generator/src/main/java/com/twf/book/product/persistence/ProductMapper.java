3d6a4ad5596ec0d5e9c37e2bf578b520c29c7ecc
tanwenfang <1643327713@qq.com>
2019-02-25 18:07:58 +0800

    删除文件

--- mybatis-generator/src/main/java/com/twf/book/product/persistence/ProductMapper.java
+++
@@ -1,7 +0,0 @@
-package com.twf.book.product.persistence;
-
-import com.twf.book.product.MyMapper;
-import com.twf.book.product.domain.Product;
-
-public interface ProductMapper extends MyMapper<Product> {
-}
